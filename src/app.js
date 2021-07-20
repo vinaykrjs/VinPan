b38fc455437d25d4140c1b9791d3f042dfd56fcb
pandukumar.gujja <pandukumar.gujja@gmail.com>
2021-07-20 10:44:22 +0530

    testing

---
+++ src/app.js
@@ -0,0 +1,13 @@
+JavaScript is the most popular and hence the most loved language around the globe. Apart from this, there are abundant reasons to learn it. Below are a listing of few important points:
+
+No need of compilers: Since JavaScript is an interpreted language, therefore it does not need any compiler for compilations.
+Used both Client and Server-side: Earlier JavaScript was used to build client-side applications only, but with the evolution of its frameworks namely Node.js and Express.js, it is now widely used for building server-side applications too.
+Helps to build a complete solution: As we saw, JavaScript is widely used in both client and server-side applications, therefore it helps us to build an end-to-end solution to a given problem.
+Used everywhere: JavaScript is so loved because it can be used anywhere. It can be used to develop websites, games or mobile apps, etc.
+Huge community support: JavaScript has a huge community of users and mentors who love this language and take it’s legacy forward.JavaScript is the most popular and hence the most loved language around the globe. Apart from this, there are abundant reasons to learn it. Below are a listing of few important points:
+
+No need of compilers: Since JavaScript is an interpreted language, therefore it does not need any compiler for compilations.
+Used both Client and Server-side: Earlier JavaScript was used to build client-side applications only, but with the evolution of its frameworks namely Node.js and Express.js, it is now widely used for building server-side applications too.
+Helps to build a complete solution: As we saw, JavaScript is widely used in both client and server-side applications, therefore it helps us to build an end-to-end solution to a given problem.
+Used everywhere: JavaScript is so loved because it can be used anywhere. It can be used to develop websites, games or mobile apps, etc.
+Huge community support: JavaScript has a huge community of users and mentors who love this language and take it’s legacy forward.
